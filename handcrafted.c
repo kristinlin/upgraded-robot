819acc3abec56b6859597f8ac8d253633b0afe14
Kristin Lin <klin11@stuy.edu>
2018-02-03 18:49:57 -0500

    tweaks

--- handcrafted.c
+++ handcrafted.c
@@ -5,24 +5,25 @@
 #include <sys/stat.h>
 #include <string.h>
 
+#define HEIGHT 512
+#define WIDTH 512
+
 int main() {
 
   //make img file
   int img = open("image.ppm", O_CREAT | O_WRONLY, 0644);
 
   //add header to img
-  int height = 512;
-  int width = 512;
   char * header;
-  sprintf(header, "P3\n%d %d\n255\n", height, width);
+  sprintf(header, "P3\n%d %d\n255\n", HEIGHT, WIDTH);
   write(img, header, strlen(header));
 
   //add pixels to img
   int row; int col;
-  for(row = 0; row < height; row++) {
-    for (col = 0; col < width; col++) {
+  for(row = 0; row < HEIGHT; row++) {
+    for (col = 0; col < WIDTH; col++) {
       char pixel[256];
-      sprintf(pixel, "0 %d %d ", row+col, width-row+col);
+      sprintf(pixel, "0 %d %d ", row+col, WIDTH-row+col);
       write(img, pixel, strlen(pixel));
     }
     // finish row; new line
@@ -32,5 +33,6 @@ int main() {
 
   //close file
   close(img);
+  printf("\nImage created!\n");
   
 }
